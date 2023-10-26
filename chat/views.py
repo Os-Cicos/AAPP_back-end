de667dee60747969f10345c943f5b90623c49587
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2023-10-26 02:22:34 -0300

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,9 @@
 from . import constants
 import os, json
 from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
 from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -24,13 +23,12 @@ class APIRootView(APIView):
         return Response(data)
 
 class assistant(APIView):
-    permission_classes = (IsAuthenticated, )
 
     def post(self, request):
         data = json.loads(request.body)
         pergunta = data.get("query")
 
-        loader = TextLoader('chat/data/data.txt')
+        loader = DirectoryLoader('chat/data/')
         documents = loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         texts = text_splitter.split_documents(documents)
@@ -38,17 +36,17 @@ class assistant(APIView):
         embeddings = OpenAIEmbeddings()
         docsearch = Chroma.from_documents(texts, embeddings)
         from langchain.prompts import PromptTemplate
-        prompt_template = """ Use the following pieces of context to answer the question. If you don't know the answer, just say that you don't know, don't try to elaborate it or hallucinate.
+        prompt_template = """ Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
         
         {context}
         Answer questions only if the information about the question is inside the documents,  if you don't find, dont try to elaborate it,  just tell that the information was not in the database. 
-        Don't answer anything other than what you have in the database or in documents. Just copy and paste what is in the loaded data!
+        Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
         You are a virtual assistant from the company BRISA.
         Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
         Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
 
         Question: {question}
-        Your answer has to be an output less than 300 characters lenght.
+        Your answer has to be an output less than 400 characters lenght.
         Answer only in Brazilian Portuguese (PT-BR)."""
         PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
