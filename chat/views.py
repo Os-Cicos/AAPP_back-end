9b49824f7f370700ffa9374055f1baa5896c71b3
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2023-11-27 19:42:32 -0300

    Update views.py
    otimização do código para carregamento dos arquivos

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,5 @@
 from . import constants
-import os
-import json
-import base64
+import os, json, base64
 from langchain.chat_models import ChatOpenAI
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,71 +8,69 @@ from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-from rest_framework.reverse import reverse
+from langchain.prompts import PromptTemplate
 import pyttsx3
+
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-
-class APIRootView(APIView):
-    def get(self, request, format=None):
-        data = {
-            "assistant": reverse("assistant", request=request, format=format),
-        }
-        return Response(data)
-
+texts = None
+docsearch = None
+prompt_template = None
 
 class assistant(APIView):
-    def post(self, request):
-        data = json.loads(request.body)
-        pergunta = data.get("query")
 
-        loader = DirectoryLoader('chat/data/')
-        documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-        texts = text_splitter.split_documents(documents)
+    def __init__(self):
+        global texts, docsearch, prompt_template
+        super().__init__()
+        if texts is None or docsearch is None or prompt_template is None:
+            loader = DirectoryLoader('chat/data/')
+            documents = loader.load()
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+            texts = text_splitter.split_documents(documents)
+            embeddings = OpenAIEmbeddings()
+            docsearch = Chroma.from_documents(texts, embeddings)
+            prompt_template = """Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
+            
+            {context}
+            Answer questions only if the information is inside the documents, if you don't find, dont try to elaborate it, just tell that the information was not in the database. 
+            Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
+            You are a virtual assistant from the company BRISA, Called "Ciçin", your pronoun is masculine.
+            Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
+            Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
 
-        embeddings = OpenAIEmbeddings()
-        docsearch = Chroma.from_documents(texts, embeddings)
-        
-        from langchain.prompts import PromptTemplate
-        prompt_template = """Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
-        
-        {context}
-        Answer questions only if the information is inside the documents, if you don't find, dont try to elaborate it, just tell that the information was not in the database. 
-        Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
-        You are a virtual assistant from the company BRISA, Called "Ciçin", your pronoun is masculine.
-        Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
-        Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
-
-        Question: {question}
-        Your answer has to be an output less than 600 characters length.
-        Answer only in Brazilian Portuguese (PT-BR)."""
-        
+            Question: {question}
+            Your answer has to be an output less than 500 characters lenght.
+            Answer only in Brazilian Portuguese (PT-BR)."""
         PROMPT = PromptTemplate(
-            template=prompt_template, input_variables=["context", "question"]
+        template=prompt_template, input_variables=["context", "question"]
         )
         chain_type_kwargs = {"prompt": PROMPT}
-        qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
-        result = qa.run(pergunta)
+        self.qa = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
+
+    def post(self, request):
+        global texts, docsearch, prompt_template
+        data = json.loads(request.body)
+        pergunta = data.get("query")
         
-        # TEXT TO SPEECH
+        
+        result = self.qa.run(pergunta)
+        
+        # TEXTO PARA ÁUDIO
 
         engine = pyttsx3.init()
         voices = engine.getProperty('voices')
-        engine.setProperty('rate', 180)  
+        engine.setProperty('rate', 200)  # velocidade 120 = lento
         for indice, vozes in enumerate(voices): 
             print(indice, vozes.name)
-        voz = 2  # Preencher de acordo com o print dos índices das vozes
+        voz = 3 
         engine.setProperty('voice', voices[voz].id)
-        
-        # Save the audio file
+
         audio_file_path = 'result.mp3'
-        engine.save_to_file(result, audio_file_path)
+        engine.save_to_file(result, 'result.mp3')
         engine.runAndWait()
         engine.stop()
 
-        # Read the audio file and encode as base64
         with open(audio_file_path, "rb") as audio_file:
             audio_data = base64.b64encode(audio_file.read()).decode("utf-8")
 
