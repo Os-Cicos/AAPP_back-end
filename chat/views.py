85159a56f85e2f8d449be471d80fded74194aa28
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2023-11-28 13:26:00 -0300

    carregamento de arquivos ao inicio do servidor

--- chat/views.py
+++ chat/views.py
@@ -14,45 +14,36 @@ import pyttsx3
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-texts = None
-docsearch = None
-prompt_template = None
+loader = DirectoryLoader('chat/data/')
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+texts = text_splitter.split_documents(documents)
+embeddings = OpenAIEmbeddings()
+docsearch = Chroma.from_documents(texts, embeddings)
+prompt_template = """Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
+
+{context}
+Answer questions only if the information is inside the documents, if you don't find, dont try to elaborate it, just tell that the information was not in the database. 
+Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
+You are a virtual assistant from the company BRISA, Called "Ciçin", your pronoun is masculine.
+Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
+Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
+
+Question: {question}
+Your answer has to be an output less than 500 characters lenght.
+Answer only in Brazilian Portuguese (PT-BR)."""
 
 class assistant(APIView):
-
-    def __init__(self):
-        global texts, docsearch, prompt_template
-        super().__init__()
-        if texts is None or docsearch is None or prompt_template is None:
-            loader = DirectoryLoader('chat/data/')
-            documents = loader.load()
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-            texts = text_splitter.split_documents(documents)
-            embeddings = OpenAIEmbeddings()
-            docsearch = Chroma.from_documents(texts, embeddings)
-            prompt_template = """Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
-            
-            {context}
-            Answer questions only if the information is inside the documents, if you don't find, dont try to elaborate it, just tell that the information was not in the database. 
-            Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
-            You are a virtual assistant from the company BRISA, Called "Ciçin", your pronoun is masculine.
-            Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
-            Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
-
-            Question: {question}
-            Your answer has to be an output less than 500 characters lenght.
-            Answer only in Brazilian Portuguese (PT-BR)."""
-        PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-        )
-        chain_type_kwargs = {"prompt": PROMPT}
-        self.qa = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
 
     def post(self, request):
         global texts, docsearch, prompt_template
         data = json.loads(request.body)
         pergunta = data.get("query")
-        
+        PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+        )
+        chain_type_kwargs = {"prompt": PROMPT}
+        self.qa = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
         
         result = self.qa.run(pergunta)
         
