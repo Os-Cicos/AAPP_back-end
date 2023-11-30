222e81e4b2aa23f2546bf2f478c3f2fcc322957b
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2023-11-30 07:31:11 -0300

    implementação do "Retrieval-augmented generation (RAG)" + OpenAI TTS

--- chat/views.py
+++ chat/views.py
@@ -1,66 +1,66 @@
 from . import constants
 import os, json, base64
+from openai import OpenAI
 from langchain.chat_models import ChatOpenAI
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from langchain.document_loaders import DirectoryLoader
-from langchain.chains import RetrievalQA
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.schema.runnable import RunnablePassthrough
 from langchain.prompts import PromptTemplate
-import pyttsx3
-
+from langchain.schema import StrOutputParser
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
-
 loader = DirectoryLoader('chat/data/')
 documents = loader.load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-texts = text_splitter.split_documents(documents)
+splits = text_splitter.split_documents(documents)
 embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
-prompt_template = """Answer the question based only on the following context. If you don't know the answer, just say that you don't know, don't try to elaborate.
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+retriever = vectorstore.as_retriever()
 
-{context}
-Answer questions only if the information is inside the documents, if you don't find, dont try to elaborate it, just tell that the information was not in the database. 
-Don't answer anything other than what you have in the database. Just copy and paste what is in the texts!
-You are a virtual assistant from the company BRISA, Called "Ciçin", your pronoun is masculine.
+prompt_template = """Responda às perguntas com base apenas no contexto fornecido. Se não souber a resposta, informe que não possui a informação sem elaborar.
+
+{contexto}
+Responda apenas se a informação estiver nos documentos carregados. Caso não encontre, informe que a informação não está na base de dados. Não forneça respostas além do que está nos textos. Basta copiar e colar diretamente dos documentos!
+Você é um assistente virtual da empresa BRISA, chamado 'Ciçin', com pronomes masculinos.
+Se a pergunta for vaga ou não clara (por exemplo, 'Quem são eles?'), solicite esclarecimento perguntando: 'Quem são "eles"? Por favor, forneça mais detalhes específicos.'
 Após responder, sempre pergunte se o usuário tem mais alguma dúvida.
-Seja direto e responda apenas apartir dos dados carregados, você não pode responder nada além do que há nos dados.
 
-Question: {question}
-Your answer has to be an output less than 500 characters lenght.
-Answer only in Brazilian Portuguese (PT-BR)."""
+Question: {questao}
+Sua resposta deve ter menos de 400 caracteres.
+Responda apenas em Português do Brasil (PT-BR)."""
+
 
 class assistant(APIView):
 
     def post(self, request):
-        global texts, docsearch, prompt_template
+        global splits, retriever, prompt_template
         data = json.loads(request.body)
-        pergunta = data.get("query")
+        question = data.get("query")
         PROMPT = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
+            template=prompt_template, input_variables=["contexto", "questao"]
         )
-        chain_type_kwargs = {"prompt": PROMPT}
-        self.qa = RetrievalQA.from_chain_type(llm = ChatOpenAI(model_name="gpt-3.5-turbo"), chain_type="stuff", retriever=docsearch.as_retriever(), chain_type_kwargs=chain_type_kwargs)
-        
-        result = self.qa.run(pergunta)
-        
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+        rag_chain = {"context": retriever, "question": RunnablePassthrough()} | PROMPT | llm | StrOutputParser()
+        result = rag_chain.invoke(question)
+
         # TEXTO PARA ÁUDIO
 
-        engine = pyttsx3.init()
-        voices = engine.getProperty('voices')
-        engine.setProperty('rate', 200)  # velocidade 120 = lento
-        for indice, vozes in enumerate(voices): 
-            print(indice, vozes.name)
-        voz = 2 # selecione de acordo com o seu sistema
-        engine.setProperty('voice', voices[voz].id)
+        client = OpenAI()
+
+        # speech_file_path = Path('C:\Users\orea1\Desktop\apigpt\gpt').parent / "speech.mp3"
+        response = client.audio.speech.create(
+        model="tts-1",
+        voice="onyx",  # alloy / echo / fable / onyx / nova / shimmer
+        input=result
+        )
+
+        response.stream_to_file("result.mp3")
 
         audio_file_path = 'result.mp3'
-        engine.save_to_file(result, 'result.mp3')
-        engine.runAndWait()
-        engine.stop()
 
         with open(audio_file_path, "rb") as audio_file:
             audio_data = base64.b64encode(audio_file.read()).decode("utf-8")
