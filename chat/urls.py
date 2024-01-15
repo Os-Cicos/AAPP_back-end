7582ff56531c0ee289816e7f6dffac5b058369c9
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-01-15 17:30:20 -0300

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,8 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-     path('assistant/', views.assistant.as_view(), name='assistant'),
+     path('assistant/', views.assistant.as_view(),name='assistant'),
+     path('transcribe/', views.transcribe.as_view(),name='transcribe'),
      path('token/', 
           jwt_views.TokenObtainPairView.as_view(), 
           name ='token_obtain_pair'),
