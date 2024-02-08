b57d6491268947ba37f220b881931e3042efe1ad
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-02-08 18:38:37 -0300

    users tokens urls created

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,22 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-     path('assistant/', views.Assistant.as_view(),name='assistant'),
-     path('loader/', views.Loader.as_view(),name='Loader'),
-     path('transcribe/', views.Transcribe.as_view(),name='transcribe'),
+     path('create_user/', 
+          views.CreateUser.as_view(), 
+          name='create_auth'),
+     path('assistant/', 
+          views.Assistant.as_view(),
+          name='assistant'),
+     path('loader/', 
+          views.Loader.as_view(),
+          name='Loader'),
+     path('transcribe/', 
+          views.Transcribe.as_view(),
+          name='transcribe'),
+     path('token/', 
+          jwt_views.TokenObtainPairView.as_view(), 
+          name ='token_obtain_pair'),
+     path('token/refresh/', 
+          jwt_views.TokenRefreshView.as_view(), 
+          name ='token_refresh'),
 ]
