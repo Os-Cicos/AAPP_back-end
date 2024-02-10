d5a61afa14fa884bc63d05d1136a35f17556116a
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-02-10 02:12:52 -0300

    idUser parameter created

--- chat/views.py
+++ chat/views.py
@@ -1,9 +1,11 @@
+import time
 from . import constants
-import os, json, base64, whisper, torch
+import os, json, base64, whisper, torch, datetime
 from openai import OpenAI
 from langchain.chat_models import ChatOpenAI
-from django.contrib.auth.models import User
-from django.core.cache import cache
+from django.utils import timezone
+from django.shortcuts import get_object_or_404
+from .models import User
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -18,16 +20,6 @@ from langchain.embeddings import HuggingFaceEmbeddings
 
 os.environ["OPENAI_API_KEY"] = constants.APIKEY
 
-class CreateUser(APIView):
-
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        if not username or not password:
-            return Response({'error': 'Username and password required'}, status=400)
-        User.objects.create_user(username, password=password)
-        return Response({'message': 'User created successfully'}, status=201)
-    
 class Loader(APIView):
 
     rag_chain = None
@@ -42,7 +34,7 @@ class Loader(APIView):
         ]
 
         if index < 0 or index >= len(directories):
-            return Response({"error": "Índice inválido"}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": "Índice inválido"}, status.HTTP_404_NOT_FOUND)
 
         try: 
             loader = S3FileLoader(*directories[index])
@@ -66,13 +58,13 @@ class Loader(APIView):
             PROMPT = PromptTemplate(
                         template=prompt_template, input_variables=["contexto", "questao"]
                     )
-            llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature= 0.0)
+            llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature= 0.5)
             Loader.rag_chain = {"contexto": retriever, "questao": RunnablePassthrough()} | PROMPT | llm | StrOutputParser()
 
             return Response({"message": "Dados carregados com sucesso!"})
         
         except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_404_NOT_FOUND)
+            return Response({"error": str(e)}, status.HTTP_404_NOT_FOUND)
 
      
 
@@ -98,29 +90,45 @@ def text_to_audio(result):
 
 class Assistant(APIView):
 
-    permission_classes = (IsAuthenticated)
+    # permission_classes = (IsAuthenticated)
 
-    def post(self, request):
+    def post(self, request, idUser= None):
         
-        token = request.META.get('HTTP_AUTHORIZATION', "").split()[1]
-        counter = cache.get(token, 0)
-        if counter >= 3:
-            return Response({"error": "Limite de requisições atingido"}, status=status.HTTP_429_TOO_MANY_REQUESTS)
-        cache.set(token, counter + 1, timeout=10800)
+        max_count = 5
+        reset_time = datetime.timedelta(minutes=1)
 
-        data = json.loads(request.body)
-        question = data.get("query")
-        use_audio = data.get("use_audio") 
-        
-        if Loader.rag_chain is None:
-            return Response({"error": "Nenhum dado foi carregado ainda."}, status=400)
-        result = Loader.rag_chain.invoke(question)
+        idUser = request.GET.get('idUser')
+        if idUser is not None: 
+            user, created = User.objects.get_or_create(idUser=idUser)
+            now = timezone.now()
+            if created:
+                user.count = 0
+                user.start_time = timezone.now()
+            if user.count >= max_count and now - user.start_time < reset_time:
+                return Response({"error": "Limite de requisições atingido"}, status.HTTP_429_TOO_MANY_REQUESTS)
+            
+            if now - user.start_time > reset_time:
+                user.count = 0
+            
+            user.count += 1
+            user.start_time = now
+            user.save()
 
-        if use_audio:
-            audio_data = text_to_audio(result)
-            return Response({"response_text": result, "response_audio": audio_data})
+            data = json.loads(request.body)
+            question = data.get("query")
+            use_audio = data.get("use_audio") 
+            
+            if Loader.rag_chain is None:
+                return Response({"error": "Nenhum dado foi carregado ainda."}, status.HTTP_400_BAD_REQUEST)
+            result = Loader.rag_chain.invoke(question)
+
+            if use_audio:
+                audio_data = text_to_audio(result)
+                return Response({"response_text": result, "response_audio": audio_data})
+            else:
+                return Response({"response_text": result})
         else:
-            return Response({"response_text": result})
+            return Response({"error": "idUser é necessário"}, status.HTTP_400_BAD_REQUEST)
         
 class Transcribe(APIView):
     
