7e6b7df8b2225ff16724ffb62d350c8081ae7678
Matheus Rennan <134556078+orea147@users.noreply.github.com>
2024-02-10 02:13:22 -0300

    Update models.py

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    idUser = models.CharField(max_length=100, unique=True)
+    start_time = models.DateTimeField(auto_now_add=True)
+    count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"idUser: {self.idUser}"
